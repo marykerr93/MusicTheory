74bec44a5a42713f704da04ff3f465454be4e290
Mary Kerr <marykerr93@gmail.com>
2020-09-18 14:01:55 -0600

    formats document

--- app.js
+++ app.js
@@ -5,41 +5,41 @@ const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 //ROUTES
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.render("home");
 });
 
-app.get("/Lessons", function(req, res){
+app.get("/Lessons", function (req, res) {
     res.render("Lessons");
 });
 
-app.get("/Lesson0", function(req, res){
+app.get("/Lesson0", function (req, res) {
     res.render("Lesson0");
 });
 
-app.get("/Lesson1", function(req, res){
+app.get("/Lesson1", function (req, res) {
     res.render("Lesson1");
 });
 
-app.get("/Lesson2", function(req, res){
+app.get("/Lesson2", function (req, res) {
     res.render("Lesson2");
 });
 
-app.get("/Lesson3", function(req, res){
+app.get("/Lesson3", function (req, res) {
     res.render("Lesson3");
 });
 
-app.get("/Contact", function(req, res){
+app.get("/Contact", function (req, res) {
     res.render("Contact");
 });
 
-app.post("/Contact", function(req, res){
+app.post("/Contact", function (req, res) {
     const smtpTrans = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 587,
@@ -50,7 +50,7 @@ app.post("/Contact", function(req, res){
         }
     });
 
-    if((req.body.website).length != 0) {
+    if ((req.body.website).length != 0) {
         res.render("home");
     } else {
 
@@ -61,9 +61,9 @@ app.post("/Contact", function(req, res){
             text: `${req.body.name} (${req.body.email}) says ${req.body.message}`
         }
 
-        smtpTrans.sendMail(mailOpts, function(err) {
+        smtpTrans.sendMail(mailOpts, function (err) {
             console.log(err);
-            if(err) {
+            if (err) {
                 res.render("contact");
             } else {
                 res.render("home");
@@ -72,15 +72,15 @@ app.post("/Contact", function(req, res){
     }
 });
 
-app.get("/Sources", function(req, res){
+app.get("/Sources", function (req, res) {
     res.render("Sources");
 });
 
-app.get("*", function(req, res){
+app.get("*", function (req, res) {
     res.render("ComingSoon");
 });
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server is running!");
 });
 
