09293bb2e8d3f66f204d6404caaaccbd8d8d40ae
Mary Kerr <marykerr93@gmail.com>
2020-09-18 13:59:25 -0600

    update fallback port

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.get("*", function(req, res){
     res.render("ComingSoon");
 });
 
-app.listen(process.env.PORT, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running!");
 });
 
